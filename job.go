83ce39b04cc317bacdee3d9a6dbff614e7e5f05e
MaheshRKumawat <maheshkumawat0911@gmail.com>
2023-04-16 01:32:41 +0530

    Auth fix

--- job.go
+++ job.go
@@ -6,19 +6,15 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/IBM/code-engine-go-sdk/codeenginev2"
-	"github.com/IBM/go-sdk-core/core"
+	"github.com/IBM/go-sdk-core/v5/core"
 )
 
-func generateJob(filename string, projectID string, ceClient *codeenginev2.NewCodeEngineV2) {
+func generateJob(filename string, projectID string, ceClient *codeenginev2.CodeEngineV2) {
 	j := PiedPiper{}
 	j.GetConf(filename)
-
-	var codeEngineService *codeenginev2.CodeEngineV2
-
-	// projectID, region := getBluemixConf()
-	// ceClient := auth(region)
 
 	createBuildOptions := ceClient.NewCreateBuildOptions(
 		projectID,
@@ -29,22 +25,22 @@ func generateJob(filename string, projectID string, ceClient *codeenginev2.NewCo
 		"dockerfile",
 	)
 
-	build, _, err := codeEngineService.CreateBuild(createBuildOptions)
+	build, _, err := ceClient.CreateBuild(createBuildOptions)
 
 	if err != nil {
 		panic(err)
 	}
 	b, _ := json.MarshalIndent(build, "", "  ")
-	fmt.Println(string(b))
+	log.Println(string(b))
 }
 
-func auth(region string) (ceClient *codeenginev2.NewCodeEngineV2) {
-	// Initialize the IAM authenticator using an API key
-	authenticator := &core.IamAuthenticator{
-		ApiKey:       os.Getenv("CE_API_KEY"),
-		ClientId:     "bx",
-		ClientSecret: "bx",
-		URL:          "https://iam.cloud.ibm.com",
+func auth(region string) (ceClient *codeenginev2.CodeEngineV2) {
+	authenticator, err := core.NewIamAuthenticatorBuilder().
+		SetApiKey(os.Getenv("CE_API_KEY")).
+		Build()
+
+	if err != nil {
+		panic(err)
 	}
 
 	ceClient, _ = codeenginev2.NewCodeEngineV2(&codeenginev2.CodeEngineV2Options{
@@ -57,7 +53,7 @@ func auth(region string) (ceClient *codeenginev2.NewCodeEngineV2) {
 
 func getBluemixConf() (projectID string, region string) {
 	home, err := os.UserHomeDir()
-	content, err := ioutil.ReadFile(home + "/.bluemix/plugins/code-engine/config.json")
+	content, err := ioutil.ReadFile(filepath.Join(home, "/.bluemix/plugins/code-engine/config.json"))
 	if err != nil {
 		log.Fatal("Error when opening file: ", err)
 	}
